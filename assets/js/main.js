c21bfb9e5edd2844744194a764cd58c17e6493fc
AdrianFigueroaAlmonacid <adri.figueroa@duocuc.cl>
2024-09-10 15:48:56 -0300

    primera version proyecto hotel

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// smooth scroll
+const links = document.querySelectorAll(".nav-base a ");
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+function clickHandler(e) {
+  e.preventDefault();
+
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+  scroll({
+    top: offsetTop - 10,
+    behavior: "smooth",
+  });
+
+}
+
