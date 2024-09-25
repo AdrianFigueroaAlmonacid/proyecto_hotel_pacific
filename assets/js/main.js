40753e86d5c2bf6ad8553283df5afef913ce78dd
AdrianFigueroaAlmonacid <adri.figueroa@duocuc.cl>
2024-09-25 12:47:37 -0300

    modificacion en paginas home, reserva

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,25 @@ function clickHandler(e) {
 
 }
 
+document.getElementById('roomForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Evita que el formulario se envíe
+
+  // Capturar los valores de los inputs
+  const destino = document.getElementById('destino').value;
+  const llegada = document.getElementById('llegada').value;
+  const salida = document.getElementById('salida').value;
+  const tipoHabitacion = document.getElementById('tipoHabitacion').value;
+  const huespedes = document.getElementById('huespedes').value;
+
+  // Crear un array con los valores
+  const datosFormulario = [
+    { campo: 'Destino', valor: destino },
+    { campo: 'Llegada', valor: llegada },
+    { campo: 'Salida', valor: salida },
+    { campo: 'Tipo Habitacion', valor: tipoHabitacion },
+    { campo: 'Huespedes', valor: huespedes }
+  ];
+
+  // Mostrar los datos en consola (puedes usarlos como necesites)
+  console.log(datosFormulario);
+});
